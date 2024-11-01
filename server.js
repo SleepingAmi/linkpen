e98945477ae9f50574a5f4d85f1b1462bca0e327
Ami <143962821+SleepingAmi@users.noreply.github.com>
2024-11-01 20:53:40 +0100

    Initial Commit: Base App (landing)
    This commit includes:
    - Nothing of significance
    - One Base App that only loads one route (/)
    - Boilerplate Code & Documentation
    - One Landing Page
    - Dependency Lists via package.json & package-lock.json
    - One "Global Variables" (global-variables.json) boilerplate

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const ejs = require('ejs');
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const { rootDomain, hostPort, siteTitle, discordInvite } = require('./global-variables.json')
+
+const port = hostPort || 8800;
+
+// kickstart express
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Cache-Control", "no-cache");
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// listen to app
+app.listen(port, function(){
+    console.log('Online: ' + port);
+})
+
+
+app.get('/', async (req, res) => {
+    res.render('pages/index',{
+        siteTitle,
+        discordInvite
+    })
+})
+
+/*
+    TO-DO:
+        - CUSTOM REDIRECTS FOR /AUTH AND OTHER APP ROUTES
+        - ACCOUNTS AND DATABASE
+*/
+app.get('/:id', async (req, res) => {
+    /*if (req.params.id === 'auth') {
+        res.render('pages/auth')
+    } else {*/
+        res.render('pages/index', {
+            siteTitle,
+            discordInvite
+        })
+    //}
+})
